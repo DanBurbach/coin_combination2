00d30271284ebbc2848b639acb466f30641d3999
Daniel Burbach and Quinn Miller <daniel@>
2018-11-15 09:41:45 -0800

    changed test and ran successfully shows sentance

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,6 +1,13 @@
 class Float
-  def make_change
-    quarter = self / 0.25
-    return quarter.to_i
+  def make_quarters
+
+    quarters = self / 0.25
+    quarters_string = quarters.to_i.to_s + " quarters"
+    #quarters_remainder = self % 0.25
+    #dimes = quarter_remainder / 0.10
+    #dimes_remainder = dimes % 0.10
+
+    return quarters_string
+
   end
 end
--- spec/coin_combinaitons_spec.rb
+++ spec/coin_combinaitons_spec.rb
@@ -4,7 +4,10 @@ require('pry')
 
 
 describe '#make_change' do
-  it("given a number, it devides the number by 25") do
-    expect(1.00.make_change).to eql(4)
+  it("given a number, it devides the number by 25 and returns a sentence") do
+    expect(1.00.make_quarters).to eql("4 quarters")
   end
+  #it("given a number, it devides by 25 and returns remainder") do
+    #expect(1.10.make_quarters).to eql("four quarters and one dime")
+  #end
 end
