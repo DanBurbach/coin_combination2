562b13e1b30a2f417f096a139804ba673ace7d8d
Daniel Burbach and Quinn Miller <daniel@>
2018-11-15 10:12:33 -0800

    re wrote method and made our second test pass again

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -7,19 +7,19 @@ class Float
       dimes = quarters_remainder / 0.10
       dimes_remainder = quarters_remainder % 0.10
       dimes_string = dimes.to_i.to_s + " dimes "
-      final_sentence = final_string.concat(dimes_string)
-      if dimes_remainder != 0
-        nickels = dimes_remainder / 0.05
-        nickels_remainder = dimes_remainder % 0.05
-        nickels_string = nickels.to_i.to_s + " dimes "
-        final_sentence = quarters_string.concat(dimes_string)
-        if nickels_remainder != 0
-          pennies = nickels_remainder / 0.01
-          pennies_remainder = pennies % 0.01
-          pennies_string = pennies.to_i.to_s + " dimes "
-          final_sentence = quarters_string.concat(dimes_string)
-        end
-      end
+      final_string.concat(dimes_string)
+      # if dimes_remainder != 0
+      #   nickels = dimes_remainder / 0.05
+      #   nickels_remainder = dimes_remainder % 0.05
+      #   nickels_string = nickels.to_i.to_s + " dimes "
+      #   final_sentence = quarters_string.concat(dimes_string)
+      #   if nickels_remainder != 0
+      #     pennies = nickels_remainder / 0.01
+      #     pennies_remainder = pennies % 0.01
+      #     pennies_string = pennies.to_i.to_s + " dimes "
+      #     final_sentence = quarters_string.concat(dimes_string)
+      #   end
+      # end
       else
         return final_string
     end
