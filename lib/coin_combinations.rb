02309f5300112e2370941f5ab12a17ec6b61039b
Daniel Burbach and Quinn Miller <daniel@>
2018-11-15 10:05:07 -0800

    re wrote method and made our first test pass again

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,13 +1,27 @@
 class Float
   def make_quarters
-
     quarters = self / 0.25
-    quarters_string = quarters.to_i.to_s + " quarters"
-    #quarters_remainder = self % 0.25
-    #dimes = quarter_remainder / 0.10
-    #dimes_remainder = dimes % 0.10
-
-    return quarters_string
-
+    quarters_remainder = self % 0.25
+    final_string = quarters.to_i.to_s + " quarters "
+    if quarters_remainder != 0
+      dimes = quarters_remainder / 0.10
+      dimes_remainder = quarters_remainder % 0.10
+      dimes_string = dimes.to_i.to_s + " dimes "
+      final_sentence = final_string.concat(dimes_string)
+      if dimes_remainder != 0
+        nickels = dimes_remainder / 0.05
+        nickels_remainder = dimes_remainder % 0.05
+        nickels_string = nickels.to_i.to_s + " dimes "
+        final_sentence = quarters_string.concat(dimes_string)
+        if nickels_remainder != 0
+          pennies = nickels_remainder / 0.01
+          pennies_remainder = pennies % 0.01
+          pennies_string = pennies.to_i.to_s + " dimes "
+          final_sentence = quarters_string.concat(dimes_string)
+        end
+      end
+      else
+        return final_string
+    end
   end
 end
--- spec/coin_combinaitons_spec.rb
+++ spec/coin_combinaitons_spec.rb
@@ -5,9 +5,12 @@ require('pry')
 
 describe '#make_change' do
   it("given a number, it devides the number by 25 and returns a sentence") do
-    expect(1.00.make_quarters).to eql("4 quarters")
+    expect(1.00.make_quarters).to eql("4 quarters ")
   end
-  #it("given a number, it devides by 25 and returns remainder") do
-    #expect(1.10.make_quarters).to eql("four quarters and one dime")
-  #end
+  it("tells us how many quarters and dimes there are") do
+    expect(1.10.make_quarters).to eql("4 quarters 1 dimes ")
+  end
+  it("tells us how many quarters, dimes and nickels there are") do
+    expect(1.15.make_quarters).to eql("4 quarters 1 dimes 1 nickels ")
+  end
 end
