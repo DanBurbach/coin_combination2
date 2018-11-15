5b999c1343a0c61883dbcae129aeefeaa932f846
Daniel Burbach and Quinn Miller <quinn@>
2018-11-15 11:33:57 -0800

    we re wrote part of our code and our first test is now passing agian

--- spec/coin_combinaitons_spec.rb
+++ spec/coin_combinaitons_spec.rb
@@ -1,16 +1,15 @@
-require('bigdecimal')
 require('rspec')
-require('coin_combinations')
+require('coin_combinations.rb')
 require('pry')
 
 
 describe '#make_change' do
   it("given a number, it devides the number by 25 and returns a sentence") do
-    expect(1.10.make_quarters).to eql("4 quarters ")
+    expect(1.00.make_quarters).to eql("4 quarters ")
   end
-  # it("tells us how many quarters and dimes there are") do
-  #   expect(1.10.make_quarters).to eql("4 quarters 1 dimes ")
-  # end
+  it("tells us how many quarters and dimes there are") do
+    expect(1.10.make_quarters).to eql("4 quarters 1 dimes ")
+  end
   # it("tells us how many quarters, dimes and nickels there are") do
   #   expect(1.15.make_quarters).to eql("4 quarters 1 dimes 1 nickels ")
   # end
